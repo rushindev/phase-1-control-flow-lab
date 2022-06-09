ad8e64fed68d89ced016d6429cf03386c1ea52e6
York Rushin <rushinyork@gmail.com>
2022-06-08 21:17:16 -0700

    commit tree a60fb214e2cc30daf060f8ecb69f9f440661ba53

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue) {
   // Write your code here!
+  let message
+  if (someValue <= 400) {
+    message = 'This one is on me!';
+  } else if (someValue <= 2000) {
+    message = 'That will be twenty bucks.';
+  } else if (someValue <= 2500) {
+    message = 'I will gladly take your thirty bucks.';
+  } else if (someValue > 2500) {
+    message = "No can do.";
+  }
+  return message
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityCheck) {
   // Write your code here!
+  let ternCity = cityCheck === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+  return ternCity
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  let tipMessage;
+
+  switch (tip) {
+    case 'generous':
+      tipMessage = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      tipMessage = 'Thank you.';
+      break;
+    default:
+      tipMessage = 'Bye.';
+      break;
+  } return tipMessage
 }
